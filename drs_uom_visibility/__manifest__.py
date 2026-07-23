5a3006eb5f64e65eabce9fe539b2d6a1c8e6dbf6
Alejandro (Drasi) <alejandro@drasi.mx>
2026-07-22 21:17:28 -0600

    [ADD] nuevo modulo para limitar visibilidad de unidades de medida

---
+++ drs_uom_visibility/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "DRS - UoM Visibility",
+    "version": "19.0.1.0.0",
+    "summary": "Restrict unit of measure form visibility.",
+    "author": "Drasi Consulting",
+    "category": "Extra Tools",
+    "license": "LGPL-3",
+    "depends": ["product"],
+    "data": [
+        "security/groups.xml",
+        "views/uom_uom_views.xml",
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
